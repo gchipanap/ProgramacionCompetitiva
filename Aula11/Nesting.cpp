b47ada11a5fe592ac7181c53fb7350c6ef8e8b62
Gabriela <64268942+gchipanap@users.noreply.github.com>
2021-11-21 17:23:40 -0500

    Update Nesting.cpp

--- Aula11/Nesting.cpp
+++ Aula11/Nesting.cpp
@@ -1,25 +1,19 @@
 int solution(vector<int> &s) 
 {
-    if (s.empty()) 
-	return 1;
-
-    if (s.length() % 2)
-	return 0;
-
-    int left_brackets = 0;
-    for(auto c: s) {
-        if(c == '(') {
-            left_brackets ++;
-        }
-        else if(left_brackets > 0) {
-            left_brackets --;
-        }
-        else {
-            return 0;
-        }
-    }
-    if(left_brackets)
-	return 0;
-    else
-	return 1;
+	stack<char> pila;
+	for(char n:S){
+		if(n=='(' )
+			pila.push(n);
+		else if(!pila.empty()){
+			if(n==')' && pila.top() == '(') 
+				pila.pop();
+		}
+		else
+			pila.push(n);
+	}
+	
+	if(pila.empty())
+		return 1;
+	else
+		return 0;
 }
