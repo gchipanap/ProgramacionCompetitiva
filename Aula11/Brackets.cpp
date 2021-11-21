d1470046b5ac550d65db9e607e14493e152bbe88
Gabriela <64268942+gchipanap@users.noreply.github.com>
2021-11-21 17:17:33 -0500

    Update Brackets.cpp

--- Aula11/Brackets.cpp
+++ Aula11/Brackets.cpp
@@ -1,4 +1,6 @@
-int solution(string &S) {
+#include <stack>
+
+int solution(string &s) {
     if (s.empty()) { return 1; }
 
     auto tam_arr = s.length();
@@ -7,11 +9,11 @@ int solution(string &S) {
 
     std::stack<char> s_stack;
 
-    auto no_pair = [&stack](char c) {
-        if (stack.empty() || stack.top() != c) {
+    auto no_pair = [&s_stack](char c) {
+        if (s_stack.empty() || s_stack.top() != c) {
             return true;
         }
-        stack.pop();
+        s_stack.pop();
         return false;
     };
 
@@ -27,10 +29,10 @@ int solution(string &S) {
                 if (no_pair('{')) { return 0; }
                 break;
             default:
-                stack.push(c);
+                s_stack.push(c);
         }
     }
-    if(stack.empty())
+    if(s_stack.empty())
 	return 1;
     else
 	return 0;
