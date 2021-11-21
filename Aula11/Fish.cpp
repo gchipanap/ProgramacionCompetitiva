bf66da01e6553b751d68281dfae1d20c788508b0
Gabriela <64268942+gchipanap@users.noreply.github.com>
2021-11-21 17:20:11 -0500

    Update Fish.cpp

--- Aula11/Fish.cpp
+++ Aula11/Fish.cpp
@@ -1,20 +1,27 @@
 int solution(vector<int> &A, vector<int> &B) {
-    const auto UPSTREAM = 0;
-    int left = 0, fish_num = A.size();
-    std::stack<int> stack;
-
-    for(int i = 0; i < fish_num; ++i) {
-        if(UPSTREAM == B[i]) {
-            while(!stack.empty() && (stack.top() < fish[i])) {
-                stack.pop();
-            }
-            if(stack.empty()) {
-                left++;
-            }
-        }
-        else {
-            stack.push(fish[i]);
-        }
-    }
-    return left + stack.size();
+ int ans=0;
+	int n=A.size();
+	stack<int> pila;
+	for(int i=0; i<n; i++){
+		int pez = A[i];
+		int dir = B[i];
+		if(dir == 0){
+			bool surv = true;
+			while(!pila.empty()){
+				int pezAux = pila.top();
+				pila.pop();
+				if(pezAux > pez){
+					pila.push(pezAux);
+					surv = false;
+					break;
+				}
+			}
+			if(surv)
+				ans++;
+		}
+		else
+			pila.push(pez);
+	}
+	ans += pila.size();
+	return ans;
 }
