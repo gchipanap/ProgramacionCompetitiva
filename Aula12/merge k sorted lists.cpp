04ba78d88d352a0aa431214ada56060e5fa0d45f
Gabriela <64268942+gchipanap@users.noreply.github.com>
2021-11-21 16:54:32 -0500

    Update merge k sorted lists.cpp

--- Aula12/merge k sorted lists.cpp
+++ Aula12/merge k sorted lists.cpp
@@ -2,27 +2,36 @@
 using namespace std;
 
 
-void  sol(std::vector<std::vector<int>> t)
-{
-  std::vector<int> tmp;
-
-  for(auto i : t)
-    for(auto j : i)
-      tmp.push_back(j);
-
-  std::priority_queue<int, std::vector<int>,std::greater<int>> s(tmp.begin(), tmp.end());
-  
-  for (; !s.empty(); s.pop())
-  {
-      std::cout << s.top() << ' ';
-  }
-}
-
-
-int main()
-{
-  std::vector<std::vector<int>> t{{1,4,5},{1,3,4},{2,6}};
-  sol(t);
-  
-  return 0;
-}
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(lists.size() <= 0)
+		return NULL;
+    
+    priority_queue<int, vector<int>, greater<int>> pq;
+    for(auto i : lists){
+        ListNode *tmp = i;
+        while(tmp != NULL){
+            pq.push(tmp->val);
+            tmp = tmp->next;
+        }
+    }
+    
+    if(pq.empty())
+        return NULL;
+    
+    ListNode *aux2 = new ListNode(pq.top());
+    pq.pop();
+    ListNode *aux3 = aux2;
+    while(!pq.empty()){
+        ListNode *n = new ListNode(pq.top());
+        aux3->next = n;
+        pq.pop();
+        aux3 = aux3->next;
+    }
+    
+    aux3->next = NULL;
+    
+    return aux2;
+    }
+};
